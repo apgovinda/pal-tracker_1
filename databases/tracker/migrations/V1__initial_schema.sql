f3388c45da767ac2d5e857f95dd6e29e58e69dd4
PAL-7 <apgovinda@gmail.com>
2019-02-13 15:53:35 +0530

    Modified schema

--- databases/tracker/migrations/V1__initial_schema.sql
+++ databases/tracker/migrations/V1__initial_schema.sql
@@ -1,12 +1,10 @@
 CREATE TABLE time_entries (
-  id         BIGINT(20) NOT NULL AUTO_INCREMENT,
-  project_id BIGINT(20),
-  user_id    BIGINT(20),
+  id         BIGINT(25) NOT NULL AUTO_INCREMENT,
+  project_id BIGINT(25),
+  user_id    BIGINT(25),
   date       DATE,
   hours      INT,
-
   PRIMARY KEY (id)
 )
   ENGINE = innodb
   DEFAULT CHARSET = utf8;
-
